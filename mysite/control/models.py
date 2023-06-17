7bd9e99ab773d1e7b1cf40aeec66d5833cee3643
maurogimenezb <maurogimenezb@gmail.com>
2023-06-16 21:56:01 -0400

    commit 2

--- mysite/control/models.py
+++ mysite/control/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Empleado(models.Model):
+    TURNOS_CHOICES = (
+        ('D', 'Día'),
+        ('T', 'Tarde'),
+        ('N', 'Noche'),
+    )
+
+    nombre = models.CharField(max_length=150)
+    departamento = models.CharField(max_length=100)
+    fecha_inicio = models.DateField(default=timezone.now)
+    dias_trabajo = models.CharField(max_length=100)
+    turno = models.CharField(max_length=1, choices=TURNOS_CHOICES)
+    horario_entrada = models.TimeField()
+    horario_salida = models.TimeField()
+
+
+
+class Jornada(models.Model):
+    TIPO_MARCACION_CHOICES = (
+        ('E', 'Entrada'),
+        ('S', 'Salida'),
+    )
+
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    tipo_marcacion = models.CharField(max_length=1, choices=TIPO_MARCACION_CHOICES)
